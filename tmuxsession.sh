dffafb2da189b11453cae76acde09b84ccdc4593
blastmaster <oeste.sebastian@googlemail.com>
2013-03-10 06:02:37 +0100

    first stage
    * implement simple session manager

--- tmuxsession.sh
+++ tmuxsession.sh
@@ -2,17 +2,15 @@
 
 # script to manage my tmux session
 
-# sname=${1:-$(echo $(tmux ls) |cut -d" " -f1)}
-# echo "sname = $sname"
-# echo $(tmux ls) |cut -d" " -f1
 
 # declare a default session name which is easy to increment
 default_sname="0x01"
 
-# for i in {0..4};do 
-#     default_sname="0x0$(($default_sname+1))"
-#     echo $default_sname
-# done
+# declaring some global variables
+# a list of all sessions
+# the length of the list
+declare -a slist=( $(tmux ls -F '#{session_name}') )
+declare -i len=${#slist[@]}
 
 # works with defaults less then 10
 function new_session()
@@ -22,36 +20,35 @@ function new_session()
         tmux new-session -s $1
         return 0
     fi
-    for session in $(tmux ls -F '#{session_name}'); do
+    local sname=$(new_session_name)
+    tmux new-session -s $sname
+}
+
+function new_session_name()
+{
+    local i=1
+    for session in ${slist[@]}; do
         if [[ $session == 0x?? ]]; then
             let i=i+1
             if ! tmux has-session -t 0x0$i 2>/dev/null; then
-                tmux new-session -s 0x0$i
-                break
+               echo "0x0$i"
             fi
-            continue
         fi
     done
 }
-
-# target=$(tmux ls | dmenu -nb '#000' -nf '#ff7c00' -sf '#000' -sb '#3cff00' -l 14 | awk -F':' '{print $1}' )
-# tmux attach-session -t $target
 
 function attach_session()
 {
     local session_name=$1
     local newsession=$2
-    echo $session_name
 
     OLDIFS=${IFS}
     IFS=$'\x0a'
-    declare -a list=( $(tmux ls -F '#{session_name}') )
     declare -a attachedlist=( $(tmux ls -F '#{session_attached}') )
-    local len=${#list[@]}
     for (( i=0; i < $len; i++ )); do
-        if [[ $session_name == ${list[$i]} ]]; then
+        if [[ $session_name == ${slist[$i]} ]]; then
             if [[ ${attachedlist[$i]} -eq 1 ]]; then
-                attach_attached_session $session_name #$newsession
+                attach_attached_session $session_name $newsession
             else
                 tmux attach-session -t $session_name
             fi
@@ -63,22 +60,29 @@ function attach_session()
 function attach_attached_session()
 {
     local basesession=$1
-    local newsession="test"
+    local newsession=${2:-$(new_session_name)}
 
     tmux new-session -d -t $basesession -s $newsession
     tmux new-window
     tmux attach-session -t $newsession \; set-option destroy-unattached
 }
 
+function choose_session()
+{
+    local target=$(tmux ls |
+        dmenu -nb '#000' -nf '#ff7c00' -sf '#000' -sb '#3cff00' -l $len |
+        awk -F':' '{print $1}' )
+    attach_session $target
+}
 
-# starting default session if no session is already running
 if ! tmux -q has-session; then
     new_session $1
 else
-    while getopts a:n opt; do
+    while getopts a:nc opt; do
         case $opt in
             a) shift; attach_session "$@";;
             n) shift; new_session "$@";;
+            c) choose_session;;
         esac
     done
 fi
