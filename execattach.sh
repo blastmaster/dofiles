4f328b654529066051f7f91d91655d5b7fd71cac
blastmaster <oeste.sebastian@googlemail.com>
2013-03-16 02:17:22 +0100

    new session in dmenu

--- execattach.sh
+++ execattach.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
 
-target=$(tmux ls |
+target=$( echo -e  "$(tmux ls)\nnew session" |
     dmenu -nb '#000' -nf '#ff7c00' -sf '#000' -sb '#3cff00' -l 14 |
     awk -F':' '{print $1}' )
-exec urxvt -e bash -c "source ~/.bin/tmuxsession.sh; attach_session $target"
+if [[ $target == "new session" ]]; then
+    exec urxvt -e bash -c "source ~/.bin/tmuxsession.sh; new_session"
+else
+    exec urxvt -e bash -c "source ~/.bin/tmuxsession.sh; attach_session $target"
+fi
