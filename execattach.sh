63de53cbabef46ddfb552ace82538e9d305a717d
blastmaster <blastmaster@tuxcode.org>
2013-09-25 03:48:39 +0200

    weird changes

--- execattach.sh
+++ execattach.sh
@@ -2,7 +2,7 @@
 
 
 target=$( echo -e  "$(tmux ls)\nnew session" |
-    dmenu -nb '#000' -nf '#ff7c00' -sf '#000' -sb '#3cff00' -l 14 |
+    dmenu -nb '#000' -nf '#ff7c00' -sf '#3cff00' -sb '#000' -l 14 |
     awk -F':' '{print $1}' )
 if [[ $target == "new session" ]]; then
     exec urxvt -e bash -c "source ~/.bin/tmuxsession.sh; new_session"
---
+++ gvimrc
@@ -0,0 +1,5 @@
+" get rid of toolbar
+set guioptions=aegiLt
+set cursorline
+set showtabline=1
+set guifont=Bitstream\ Vera\ Sans\ Mono\ 7
