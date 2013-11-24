aa68365cae3cbfd592a3fb1df8c1a967d738e419
blastmaster <blastmaster@tuxcode.org>
2013-11-24 17:22:27 +0100

    using /usr/bin/zsh

--- execattach.sh
+++ execattach.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
+if [[ -z $SHELL ]]; then
+	SHELL=$(which bash)
+fi
 
 target=$( echo -e  "$(tmux ls)\nnew session" |
     dmenu -nb '#000' -nf '#ff7c00' -sf '#3cff00' -sb '#000' -l 14 |
     awk -F':' '{print $1}' )
 if [[ $target == "new session" ]]; then
-    exec urxvt -e bash -c "source ~/.bin/tmuxsession.sh; new_session"
+    exec urxvt -e ${SHELL} -c "source ~/.bin/tmuxsession.sh; new_session"
 else
-    exec urxvt -e bash -c "source ~/.bin/tmuxsession.sh; attach_session $target"
+    exec urxvt -e ${SHELL} -c "source ~/.bin/tmuxsession.sh; attach_session $target"
 fi
