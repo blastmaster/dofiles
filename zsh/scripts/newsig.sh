ff07cccc330559ca0d1375278f218db64fa446e8
blastmaster <blastmaster@tuxcode.org>
2016-11-14 08:59:13 +0100

    de-modlarize zshrc
    * having my functions and aliases together! this is easier to handle
      with.

--- zsh/scripts/newsig.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#inspired by: http://www.guckes.net/Setup/zshrc.mine
-KEYID=
-gpg --refresh-keys $KEYID 2>/dev/null
-gpg --recv-keys `gpg --list-sigs $KEYID|grep "not found"|cut -b 9-17`
-gpg --list-sigs $KEYID | sed 's/^......................//' | sort -n | tail $@
-
