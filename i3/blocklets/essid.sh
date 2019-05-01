6780864c0932f8e033acaa03e6b0a6c88c69df98
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2019-05-01 19:24:54 +0200

    [i3] Move i3blocklets to i3/blocklets
    * [essid.sh] check if wireless interface is up otherwise do not draw any block.
    * Increase update interval to 30 seconds.

--- i3/blocklets/essid.sh
+++ i3/blocklets/essid.sh
@@ -3,6 +3,9 @@
 
 IFACE=${IFACE:-wlp3s0}
 
+# Do not display this block if the wireless interface is down.
+[[ "$(cat /sys/class/net/${IFACE}/operstate)" = "down" ]] && exit
+
 essid=$(/sbin/iwconfig $IFACE | grep -i essid | cut -d '"' -f2 )
 
 echo "$essid"
